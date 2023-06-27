bc0328dd03abed14fd0fd3bb62fdb5ce0b9c2138
freddymensh <freddymensh@gmail.com>
2023-06-27 23:07:45 +0200

    started lcd implementation

--- ESP32_ArtNet_TxRx/src/main.cpp
+++ ESP32_ArtNet_TxRx/src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
+#include <time.h>
+
+void dmx_status_print(void * pvParameters);
 void artnet_u1_callback(const uint8_t* data, const uint16_t size);
 void print_dmx(byte* dmx_buffer);
-
-// LED settings
-byte LED_PINS[] = {22, 19, 23}; // pins at MCU where LEDs are connected
-#define DMX_CHANNEL_OFFSET 1    // DMX base address for LEDs
-#define N_CHANNELS 3            // number of channels occupied by LEDs
-
+void printLocalTime(bool newline=true);
 
 /*
 ** TX: The one which recieves ArtNet and sends it on DMX. The TX shows the data on its attached LEDs
@@ -19,8 +17,20 @@ byte LED_PINS[] = {22, 19, 23}; // pins at MCU where LEDs are connected
 */
 //#define ETHERNET
 
-#include "esp_dmx.h"
 
+// LED settings
+byte LED_PINS[] = {22, 19, 5}; // pins at MCU where LEDs are connected
+#define DMX_CHANNEL_OFFSET 1    // DMX base address for LEDs
+#define N_CHANNELS 3            // number of channels occupied by LEDs
+
+
+// time
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+
+
+// network
 #ifdef TX
   #ifdef ETHERNET
     #include <ArtnetEther.h>
@@ -30,19 +40,56 @@ byte LED_PINS[] = {22, 19, 23}; // pins at MCU where LEDs are connected
     
     #include <ArtnetWiFi.h>
     ArtnetWiFiReceiver artnet;
-    uint8_t universe1 = 0;
+    uint8_t universe0 = 0;
   #endif
+#else
+#endif
 
-  // debug
-  int last_dmx_print = 0;
+
+// DMX
+#include "esp_dmx.h"
+byte dmx_buffer[DMX_MAX_PACKET_SIZE] = {0};
+const dmx_port_t dmx_num = DMX_NUM_2;
+QueueHandle_t dmx_queue;
+bool dmx_available = false;
+
+#ifdef TX
+  #define DMX_SEND_INTERVAL 1000
+  int last_dmx_send = 0;
 #else
   dmx_event_t event;
   byte current_value[N_CHANNELS] = {0};
 #endif
 
-byte dmx_buffer[DMX_MAX_PACKET_SIZE] = {0};
-const dmx_port_t dmx_num = DMX_NUM_2;
-QueueHandle_t dmx_queue;
+// LCD
+#define LCD
+#ifdef LCD
+  #ifdef TX
+    //#include <Adafruit_GFX.h>
+    // //#include <Adafruit_I2CDevice.h>
+    #include <Adafruit_ST7789.h>
+
+    #define TFT_MOSI 23  // SDA Pin on ESP32
+    #define TFT_SCLK 18  // SCL Pin on ESP32
+    #define TFT_CS   15  // Chip select control pin
+    #define TFT_DC    2  // Data Command control pin
+    #define TFT_RST   4  // Reset pin (could connect to RST pin)
+
+    Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);   // Initialize Adafruit ST7789 TFT library
+    //Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST); 
+  #endif
+#endif
+
+// debug
+#ifdef TX
+  #define DMX_PRINT_INTERVAL 10000
+  int last_dmx_print = 0;
+#endif
+
+
+//////////////////////////////////////////
+///               SETUP                ///
+//////////////////////////////////////////
 
 void setup() {
   // set up serial port
@@ -67,7 +114,9 @@ void setup() {
         Serial.print(".");
         delay(250);
       }
-    Serial.print("WiFi connected, IP = ");
+    Serial.printf("\nWiFi connected. ");
+    Serial.print(WiFi.SSID());
+    Serial.print(" @ ");
     Serial.println(WiFi.localIP());
     #endif
   #endif
@@ -75,7 +124,8 @@ void setup() {
   // set up ArtNet
   #ifdef TX
     artnet.begin();
-    artnet.subscribe(universe1, artnet_u1_callback);
+    artnet.subscribe(universe0, artnet_u1_callback);
+    Serial.printf("listening to universe %d\n", universe0);
   #endif
 
   // set up dmx
@@ -96,10 +146,42 @@ void setup() {
     pinMode(LED_PINS[i], OUTPUT);
   }
 
+  // configure time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.print("set time to: ");
+  printLocalTime();
+
+  // set up LCD
+  #ifdef LCD
+    #ifdef TX
+      tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixel
+      tft.setRotation(3);
+      tft.fillScreen(ST77XX_BLACK);
+
+      tft.setTextColor(ST77XX_WHITE);
+      tft.println("hello world");
+    #endif
+  #endif
+
+  // services
+  xTaskCreatePinnedToCore (
+    dmx_status_print,     // Function to implement the task
+    "dmx status print",   // Name of the task
+    1750,                 // Stack size in bytes
+    NULL,                 // Task input parameter
+    0,                    // Priority of the task
+    NULL,                 // Task handle.
+    1                     // Core where the task should run
+  );
+
   // done
-  Serial.print("Setup done.\n");
+  Serial.print("Setup done.\n\n");
 
 }
+
+//////////////////////////////////////////
+///               LOOPS                ///
+//////////////////////////////////////////
 
 void loop() {
   #ifdef TX
@@ -107,19 +189,17 @@ void loop() {
     artnet.parse();
     
     // send data on DMX
-    dmx_write_packet(dmx_num, dmx_buffer, DMX_MAX_PACKET_SIZE);
-    dmx_send_packet(dmx_num, DMX_MAX_PACKET_SIZE);
-    dmx_wait_send_done(dmx_num, DMX_PACKET_TIMEOUT_TICK);
+    if(dmx_available || ((millis()-last_dmx_send) > DMX_SEND_INTERVAL) ){
+      dmx_available = false;
+
+      dmx_write_packet(dmx_num, dmx_buffer, DMX_MAX_PACKET_SIZE);
+      dmx_send_packet(dmx_num, DMX_MAX_PACKET_SIZE);
+      dmx_wait_send_done(dmx_num, DMX_PACKET_TIMEOUT_TICK);
+    }
 
     // display data on LEDs
     for(int i=0; i<N_CHANNELS; i++){
       analogWrite(LED_PINS[i], dmx_buffer[i+DMX_CHANNEL_OFFSET]);
-    }
-  
-    // print dmx data
-    if (millis()-last_dmx_print > 10000){
-      last_dmx_print = millis();
-      print_dmx(dmx_buffer);
     }
 
   #else
@@ -140,19 +220,41 @@ void loop() {
 
 }
 
+//////////////////////////////////////////
+
+void dmx_status_print(void * pvParameters){
+  while(true){
+    #ifdef TX
+      // print dmx data
+      if (millis()-last_dmx_print > DMX_PRINT_INTERVAL){
+        last_dmx_print = millis();
+        print_dmx(dmx_buffer);
+      }
+    #endif //ifdef TX
+  }
+}
+
+//////////////////////////////////////////
+///             FUNCTIONS              ///
+//////////////////////////////////////////
 
 void artnet_u1_callback(const uint8_t* data, const uint16_t size) {
     // copy data to buffer
     for(int i=0; i<size; i++){
       dmx_buffer[i+1] = data[i];
     }
+    // set signal
+    dmx_available = true;
 }
+
+//////////////////////////////////////////
 
 void print_dmx(byte* dmx_buffer){
   int cols = 16;
   int rows = 32;
 
-  // TODO print header
+  // print header
+  printLocalTime();
 
   // print seperator
   Serial.print("+---------++");
@@ -178,5 +280,19 @@ void print_dmx(byte* dmx_buffer){
   for(int i=0; i<cols; i++){
     Serial.print("-----+");
   }
-  Serial.println("");
+  Serial.print("\n\n");
+}
+
+//////////////////////////////////////////
+
+void printLocalTime(bool newline){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.print(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  if(newline){
+    Serial.println("");
+  }
 }
